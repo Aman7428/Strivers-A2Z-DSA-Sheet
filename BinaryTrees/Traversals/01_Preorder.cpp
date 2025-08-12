c5f3eccc84f4d7a49bc2f36c6a0b45c10ebb084b
Aman7428 <amankumarsharma2570@gmail.com>
2025-08-12 21:49:52 +0530

    Binary Tree Preorder Traversal

---
+++ BinaryTrees/Traversals/01_Preorder.cpp
@@ -0,0 +1,22 @@
+//Binary Tree Preorder Traversal (leetcode-144)
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(): val(0), left(nullptr), right(nullptr){};
+    TreeNode(int x): val(x), left(nullptr), right(nullptr){};
+    TreeNode(int x, TreeNode *left, TreeNode *right): val(x), left(left), right(right){};
+};
+void preorder(TreeNode *root, vector<int>& result){
+    if(root==nullptr)return;
+    result.push_back(root->val);
+    preorder(root->left, result);
+    preorder(root->right, result);
+}
+vector<int>PreorderTraversal(TreeNode *root){
+    vector<int>result;
+    preorder(root, result);
+}
